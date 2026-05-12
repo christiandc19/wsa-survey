06e52342f6feb0e0f138af195dc950977b95029c
github_christiandc19 <christiandc19@hotmail.com>
2026-05-12 12:57:35 -0700

    configure vite.config file for widget naming

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,43 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+
   define: {
     "process.env": {},
   },
+
   build: {
     emptyOutDir: true,
+
+    // Forces a single CSS file output
     cssCodeSplit: false,
+
     lib: {
       entry: "src/widget.jsx",
+
+      // Global window object name
       name: "WebSmartAssistant",
-      fileName: () => "widget.js",
+
+      // Final JS filename
+      fileName: () => "survey-widget.js",
+
       formats: ["iife"],
+    },
+
+    rollupOptions: {
+      output: {
+        /*
+          Automatically rename the generated CSS file
+          so we no longer need to manually rename it.
+        */
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name?.endsWith(".css")) {
+            return "survey-widget.css";
+          }
+
+          return assetInfo.name;
+        },
+      },
     },
   },
 });
